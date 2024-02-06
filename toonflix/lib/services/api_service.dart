d05bf5f0db91b40a83b86925207c22b252f94211
ljy0221 <42925743+ljy0221@users.noreply.github.com>
2024-02-06 15:50:31 +0900

    Add files via upload

---
+++ toonflix/lib/services/api_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pj3/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonIstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonIstances.add(WebtoonModel.fromjson(webtoon));
+      }
+      return webtoonIstances;
+    }
+    throw Error();
+  }
+}
